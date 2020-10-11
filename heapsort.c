308006555f0fd8414fba38f8dfaf66c6695add53
Andrey <babaevwebd@gmail.com>
2020-10-11 17:14:52 +0300

    Heapsort

--- heapsort.c
+++ heapsort.c
@@ -1,19 +1,18 @@
 #include<stdio.h>
-#define BN 9
 void swap(int *C, int *D){
     int d;
     d = *C;
     *C= *D;
     *D = d;
 }
-int Heapify(int *A, int i, int n){
+void Heapify(int *A, int n, int i){ 
     int largest = i;
-    int l = 2*i;
-    int r = 2*i + 1;
-    if (l < n && A+l > A+largest){
+    int l = 2*i +1;
+    int r = 2*i + 2;
+    if (l < n && *(A+l) > *(A+largest)){
         largest = l;
     }
-    if (r < n && A+r > A+largest){
+    if (r < n && *(A+r) > *(A+largest)){
         largest = r;
     }
     if (largest != i)
@@ -23,7 +22,7 @@ int Heapify(int *A, int i, int n){
     }
 
 }
-void heapSort(int A[], int n){
+void heapSort(int A[], int n){ 
     for(int i = n/2 -1; i >=0;i--){
         Heapify(A,n,i);
     }
@@ -35,9 +34,8 @@ void heapSort(int A[], int n){
 }
 
 void printArray(int A[], int n){
-    for(int i = 0; i < n; ++i){
+    for(int i = n-1; i >= 0; i--){
         printf("%d ", A[i]);
-        printf("\n");
     }
 }
 
